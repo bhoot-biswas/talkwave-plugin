536cea2b4f133bb74856af9b9771a5dda2ab9602
Mithun Biswas <bhoot.biswas@gmail.com>
2024-10-22 01:09:22 +0600

    Refactor episode-image block to add overlay play button and image zoom effect

--- build/blocks/episode-image/render.php
+++ build/blocks/episode-image/render.php
@@ -18,6 +18,34 @@
 	if ( ! $episode_image ) {
 		return '';
 	}
+
+	$link_target    = '_self';
+	$rel 		  = '';
+	$height = '';
+	
+	// Image markup
+    $episode_image = sprintf(
+        '<img src="%1$s" alt="%2$s" class="podcast-episode-thumbnail">',
+        esc_url( $episode_image['src'] ),
+        esc_attr( $episode->post_title )
+    );
+	
+	// Overlay markup with SVG play button
+    $overlay_markup = '
+        <div class="play-icon-overlay">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="36" height="36">
+                <path d="M8 5v14l11-7z"/>
+            </svg>
+        </div>';
+		
+	printf(
+		'<a href="%1$s" target="%2$s" %3$s %4$s>%5$s%6$s</a>',
+		get_the_permalink( $episode_ID ),
+		esc_attr( $link_target ),
+		$rel,
+		$height,
+		$episode_image,
+		$overlay_markup
+	);
 	?>
-	<img src="<?php echo esc_url( $episode_image['src'] ); ?>" alt="<?php echo $episode->post_title ?>">
 </figure>
