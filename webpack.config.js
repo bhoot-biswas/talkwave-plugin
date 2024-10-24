6bec621b5fb22a08aca88af767d5962b15ff8cce
Mithun Biswas <bhoot.biswas@gmail.com>
2024-10-24 13:39:17 +0600

    Refactor webpack.config.js to update entry point for frontend JavaScript file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-const path = require( 'path' );
+const [
+	scriptConfig,
+	moduleConfig,
+] = require("@wordpress/scripts/config/webpack.config");
+const path = require("path");
 
-module.exports = {
-    ...defaultConfig,
-    entry: {
-        'block-editor': [ path.resolve( __dirname, 'src/block-editor.js' ) ],
-        'block-frontend': [ path.resolve( __dirname, 'src/block-frontend.js' ) ],
-    },
-    output: {
-        path: path.resolve( __dirname, 'build' ), filename: '[name].js',
-    },
-};
+module.exports = [
+	scriptConfig,
+	{
+		...moduleConfig,
+		entry: {
+			...moduleConfig.entry(),
+			frontend: [path.resolve(__dirname, "src/frontend.js")],
+		},
+	},
+];
