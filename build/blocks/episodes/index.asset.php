5fac96aa0db62dea01c8df10612fea8eb6ee0af1
Mithun Biswas <bhoot.biswas@gmail.com>
2024-10-26 02:09:23 +0600

    Refactor episode rendering to support series playlists

--- build/blocks/episodes/index.asset.php
+++ build/blocks/episodes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e1d83e39722714b2f1ad');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1e275c5c95cf8b0300d9');
--- src/blocks/episodes/render.php
+++ src/blocks/episodes/render.php
@@ -2,63 +2,115 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
-?>
-<div 
-	<?php echo get_block_wrapper_attributes(); ?>
-	data-wp-interactive="talkwave"
->
+
+// Arguments for querying latest episodes
+$args = array(
+	'post_type'      => 'podcast',
+	'posts_per_page' => 8,
+	'order'          => 'DESC',
+);
+
+// The query
+$latest_episodes = get_posts( $args );
+
+if ( ! $latest_episodes ) :
+	?>
+	<p><?php _e( 'No episodes found.' ); ?></p>
 	<?php
-	// Arguments for querying latest episodes
-	$args = array(
-		'post_type'      => 'podcast',
-		'posts_per_page' => 8,         // Adjust to the number of episodes you want to display
-		'order'          => 'DESC',    // Show latest first
+else :
+	$allowed_keys = array(
+		'episode_id',
+		'album_art',
+		'podcast_title',
+		'title',
+		'date',
+		'duration',
+		'excerpt',
+		'audio_file',
 	);
 
-	// The query
-	$latest_episodes = get_posts( $args );
+	$playlist_id    = 'latest-episodes';
+	$playlist_items = array();
 
-	// Check if there are any episodes
-	if ( $latest_episodes ) :
-		$allowed_keys = array(
-			'episode_id',
-			'album_art',
-			'podcast_title',
-			'title',
-			'date',
-			'duration',
-			'excerpt',
-			'audio_file',
-		);
+	if ( is_tax( 'series' ) ) {
+		$term        = get_queried_object();
+		$playlist_id = $term->slug;
+	}
 
-		$playlist_items = array();
+	foreach ( $latest_episodes as $episode ) {
+		$player_data = ssp_episode_controller()->episode_repository->get_player_data( $episode->ID );
+		$terms       = get_the_terms( $episode->ID, 'series' );
 
-		foreach ( $latest_episodes as $episode ) {
-			$player_data = ssp_episode_controller()->episode_repository->get_player_data( $episode->ID );
-			$terms       = get_the_terms( $episode->ID, 'series' );
-
-			if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
-				$podcast_title = $terms[0]->name; // Use the first available term
-			}
-
-			// Set the podcast title in player data
-			$player_data['podcast_title'] = $podcast_title;
-
-			$playlist_items[] = array_intersect_key( $player_data, array_flip( $allowed_keys ) );
+		if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+			$podcast_title = $terms[0]->name; // Use the first available term
 		}
-		?>
-		<div 
-			class="<?php echo is_archive() ? 'episode-list' : 'episode-grid'; ?>"
-			data-wp-init="callbacks.setPlaylist"
-			<?php
+
+		// Set the podcast title in player data
+		$player_data['podcast_title'] = $podcast_title;
+
+		$playlist_items[] = array_intersect_key( $player_data, array_flip( $allowed_keys ) );
+	}
+	?>
+	<div 
+		<?php echo get_block_wrapper_attributes(); ?>
+		data-wp-interactive="talkwave"
+		data-wp-init="callbacks.setPlaylist"
+		<?php
 			echo wp_interactivity_data_wp_context(
 				array(
-					'playlist_id' => 'latest-episodes',
+					'playlist_id' => $playlist_id,
 					'playlist'    => $playlist_items,
 				)
 			);
+		?>
+	>
+		<?php
+		if ( is_tax( 'series' ) ) :
+			$term        = get_queried_object();
+			$title       = $term->name;
+			$description = get_the_archive_description();
+			$image_url   = ssp_series_repository()->get_image_src( $term, 'full' );
 			?>
-		>
+			<div
+				class="podcast-item"
+				data-wp-class--is-loading="state.playlistLoading"
+				data-wp-class--is-playing="state.playlistPlaying"
+				<?php
+				echo wp_interactivity_data_wp_context(
+					array(
+						'playlist_id' => $term->slug,
+					)
+				);
+				?>
+			>
+				<div class="podcast-image-wrapper">
+					<img class="podcast-image" src="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $title ); ?>">
+				</div>
+
+				<div class="podcast-content">
+					<h2 class="series-title"><?php echo esc_html( $title ); ?></h2>
+
+					<?php if ( $description ) : ?>
+						<div class="series-description">
+							<?php echo wp_kses_post( wpautop( $description ) ); ?>
+						</div>
+					<?php endif; ?>
+
+					<div class="talkwave-buttons">
+						<button class="talkwave-button talkwave-button--play" data-wp-on--click="actions.handlePlaylist">
+							<?php echo Talkwave\Icon_Display::get_icon( 'play', array( 'aria-hidden' => 'true' ) ); ?>
+							<span>Play</span>
+						</button>
+						<button class="talkwave-button talkwave-button--pause" data-wp-on--click="actions.handlePlaylist">
+							<?php echo Talkwave\Icon_Display::get_icon( 'pause', array( 'aria-hidden' => 'true' ) ); ?>
+							<span>Pause</span>
+						</button>
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
+		
+		<div class="<?php echo is_archive() ? 'episode-list' : 'episode-grid'; ?>">
 			<?php
 			global $post;
 
@@ -127,9 +179,6 @@
 			wp_reset_postdata();
 			?>
 		</div>
-	<?php else : ?>
-		<p><?php _e( 'No episodes found.' ); ?></p>
-		<?php
-	endif;
-	?>
-</div>
+	</div>
+	<?php
+endif;
