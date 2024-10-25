5be4fc6bc488ed6b6e597e445185415cb3b166df
Mithun Biswas <bhoot.biswas@gmail.com>
2024-10-25 15:09:02 +0600

    Refactor grid layout for podcast series list

--- build/blocks/episodes/index.asset.php
+++ build/blocks/episodes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '791a3ddbfd790ba4a95a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '64521f6e362b19a230cd');
--- build/blocks/podcasts/index.asset.php
+++ build/blocks/podcasts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '09d23d8558d29015dbb6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4ac9f6ddc3257230c53a');
