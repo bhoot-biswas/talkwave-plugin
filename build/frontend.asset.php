0fcae32f22d1fdf5b10c2d033101b91f2be9dbf3
Mithun Biswas <bhoot.biswas@gmail.com>
2024-10-25 22:36:50 +0600

    Add custom scrollbar styles

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '0321852d77c0fd7d0076', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '19957d0cacbff461d55d', 'type' => 'module');
