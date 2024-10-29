7c0a4dd804702f02f32f2e14762d9cc7116d0e59
Mithun Biswas <bhoot.biswas@gmail.com>
2024-10-29 11:43:13 +0600

    Add CSS styles for episode title links to handle overflow and line clamping

--- build/blocks/episodes/index.asset.php
+++ build/blocks/episodes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c06ae8569ee4cb5e66f9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '447344527c497c270538');
--- build/blocks/podcasts/index.asset.php
+++ build/blocks/podcasts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'becd891ed6d92b77fcb5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'cea5fe3a29d1ec48db5f');
